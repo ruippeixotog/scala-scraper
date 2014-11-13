adef87af2fe01ae174df758af73665fed4e7c247
Rui Gonçalves <ruippeixotog@gmail.com>
2014-11-13 20:08:22 +0000

    Make extractor constructors return HtmlExtractor
    This is needed so that created extractors have an implicit Functor in
    scope. Functors are invariant, which means that a
    Functor[HtmlExtractor] does not work as a Functor[SimpleExtractor].

--- src/main/scala/net/ruippeixotog/scalascraper/dsl/DSL.scala
+++ src/main/scala/net/ruippeixotog/scalascraper/dsl/DSL.scala
@@ -1,11 +1,20 @@
 package net.ruippeixotog.scalascraper.dsl
 
 import net.ruippeixotog.scalascraper.scraper._
+import org.jsoup.select.Elements
 
 import scala.collection.convert.WrapAsScala
 
 object DSL extends ImplicitConversions with ScrapingOps with ConfigLoadingHelpers with WrapAsScala {
-  val extractor = SimpleExtractor
+
+  def extractor(cssQuery: String): HtmlExtractor[Seq[String]] = SimpleExtractor(cssQuery)
+
+  def extractor[C](cssQuery: String, contentExtractor: Elements => C): HtmlExtractor[C] =
+    SimpleExtractor(cssQuery, contentExtractor)
+
+  def extractor[C, A](cssQuery: String, contentExtractor: Elements => C, contentParser: C => A): HtmlExtractor[A] =
+    SimpleExtractor(cssQuery, contentExtractor, contentParser)
+
   val validator = SimpleValidator
 
   val Extract = ContentExtractors
--- src/main/scala/net/ruippeixotog/scalascraper/dsl/ImplicitConversions.scala
+++ src/main/scala/net/ruippeixotog/scalascraper/dsl/ImplicitConversions.scala
@@ -1,14 +1,16 @@
 package net.ruippeixotog.scalascraper.dsl
 
-import net.ruippeixotog.scalascraper.scraper.SimpleExtractor
+import net.ruippeixotog.scalascraper.scraper.{HtmlExtractor, SimpleExtractor}
 import net.ruippeixotog.scalascraper.util._
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 trait ImplicitConversions {
-  implicit def cssQueryAsExtractor(cssQuery: String) = SimpleExtractor(cssQuery)
 
-  implicit def contentExtractorAsExtractor[C](contentExtractor: Elements => C) = {
+  implicit def cssQueryAsExtractor(cssQuery: String): HtmlExtractor[Seq[String]] =
+    SimpleExtractor(cssQuery)
+
+  implicit def contentExtractorAsExtractor[C](contentExtractor: Elements => C): String => HtmlExtractor[C] = {
     cssQuery: String => SimpleExtractor(cssQuery, contentExtractor)
   }
 
