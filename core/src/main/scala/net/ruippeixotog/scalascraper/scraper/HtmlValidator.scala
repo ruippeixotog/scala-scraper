82e1c16e7bf0db86a5c26611fec17220d00372d1
Rui Gonçalves <ruippeixotog@gmail.com>
2017-05-16 00:13:08 +0100

    Remove remaining usages of SimpleValidator

--- core/src/main/scala/net/ruippeixotog/scalascraper/scraper/HtmlValidator.scala
+++ core/src/main/scala/net/ruippeixotog/scalascraper/scraper/HtmlValidator.scala
@@ -4,24 +4,30 @@ import net.ruippeixotog.scalascraper.model.{ ElementQuery, Element }
 
 import scala.util.Try
 
-trait HtmlValidator[-E <: Element, +R] {
+trait HtmlValidator[-E <: Element, +R] { outer =>
   def matches(doc: ElementQuery[E]): Boolean
   def result: Option[R]
+
+  def withResult[R2](res: R2): HtmlValidator[E, R2] =
+    HtmlValidator(ContentExtractors.pElements[E], res)(matches)
+
+  def withoutResult: HtmlValidator[E, Nothing] =
+    HtmlValidator(ContentExtractors.pElements[E])(matches)
 }
 
 object HtmlValidator {
 
   def apply[E <: Element, A, R](extractor: HtmlExtractor[E, A])(matcher: A => Boolean): HtmlValidator[E, R] =
-    SimpleValidator[E, A, R](extractor, matcher)
+    new HtmlValidatorImpl[E, A, R](extractor, matcher)
 
   def apply[E <: Element, A, R](extractor: HtmlExtractor[E, A], result: R)(matcher: A => Boolean): HtmlValidator[E, R] =
-    SimpleValidator[E, A, R](extractor, matcher, Some(result))
+    new HtmlValidatorImpl[E, A, R](extractor, matcher, Some(result))
 
   def apply[R](polyExtractor: PolyHtmlExtractor)(matcher: polyExtractor.Out[Element] => Boolean): HtmlValidator[Element, R] =
-    SimpleValidator[Element, polyExtractor.Out[Element], R](polyExtractor[Element], matcher)
+    new HtmlValidatorImpl[Element, polyExtractor.Out[Element], R](polyExtractor[Element], matcher)
 
   def apply[R](polyExtractor: PolyHtmlExtractor, result: R)(matcher: polyExtractor.Out[Element] => Boolean): HtmlValidator[Element, R] =
-    SimpleValidator[Element, polyExtractor.Out[Element], R](polyExtractor[Element], matcher, Some(result))
+    new HtmlValidatorImpl[Element, polyExtractor.Out[Element], R](polyExtractor[Element], matcher, Some(result))
 
   val matchAll = new HtmlValidator[Element, Nothing] {
     def matches(doc: ElementQuery[Element]) = true
@@ -42,6 +48,14 @@ object HtmlValidator {
     def matches(doc: ElementQuery[Element]) = false
     def result = Some(res)
   }
+
+  private[this] class HtmlValidatorImpl[-E <: Element, A, +R](
+      htmlExtractor: HtmlExtractor[E, A],
+      matcher: A => Boolean,
+      val result: Option[R] = None) extends HtmlValidator[E, R] {
+
+    def matches(doc: ElementQuery[E]) = Try(htmlExtractor.extract(doc)).map(matcher).getOrElse(false)
+  }
 }
 
 @deprecated("SimpleValidator is deprecated. Use HtmlValidator.apply methods instead", "2.0.0")
@@ -52,8 +66,8 @@ case class SimpleValidator[-E <: Element, A, +R](
 
   def matches(doc: ElementQuery[E]) = Try(htmlExtractor.extract(doc)).map(matcher).getOrElse(false)
 
-  def withResult[R2](res: R2) = SimpleValidator(htmlExtractor, matcher, Some(res))
-  def withoutResult = SimpleValidator(htmlExtractor, matcher, None)
+  override def withResult[R2](res: R2) = SimpleValidator(htmlExtractor, matcher, Some(res))
+  override def withoutResult = SimpleValidator(htmlExtractor, matcher, None)
 }
 
 @deprecated("SimpleValidator is deprecated. Use HtmlValidator.apply methods instead", "2.0.0")
