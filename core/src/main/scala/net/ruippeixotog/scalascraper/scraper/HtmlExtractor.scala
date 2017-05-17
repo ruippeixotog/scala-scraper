9d33f5017cd8a7607e3b769fefd313733eef2464
Rui Gonçalves <ruippeixotog@gmail.com>
2017-05-17 22:46:56 +0100

    Add Scaladoc to HtmlExtractor and PolyHtmlExtractor

--- core/src/main/scala/net/ruippeixotog/scalascraper/scraper/HtmlExtractor.scala
+++ core/src/main/scala/net/ruippeixotog/scalascraper/scraper/HtmlExtractor.scala
@@ -4,25 +4,69 @@ import scalaz.Monad
 
 import net.ruippeixotog.scalascraper.model.{ Element, ElementQuery }
 
+/**
+  * An object able to extract content from [[net.ruippeixotog.scalascraper.model.ElementQuery]] instances.
+  *
+  * @tparam E the type of the elements needed by this `HtmlExtractor`
+  * @tparam A the type of the extracted content
+  */
 trait HtmlExtractor[-E <: Element, +A] extends (ElementQuery[E] => A) {
 
-  def extract(doc: ElementQuery[E]): A
+  /**
+    * Extracts content from an `ElementQuery`.
+    *
+    * @param q the element query from which content is to be extracted
+    * @return the extracted content.
+    */
+  def extract(q: ElementQuery[E]): A
 
-  @inline final def apply(doc: ElementQuery[E]) = extract(doc)
+  @inline final def apply(q: ElementQuery[E]) = extract(q)
 
+  /**
+    * Maps a function over the extracted content of this extractor.
+    *
+    * @param f the function to map over this extractor
+    * @tparam B the output type of the function
+    * @return an `HtmlExtractor` returning the contents extracted by this extractor mapped by `f`.
+    */
   def map[B](f: A => B): HtmlExtractor[E, B] =
-    HtmlExtractor { doc => f(extract(doc)) }
+    HtmlExtractor { q => f(extract(q)) }
 
+  /**
+    * Applies a CSS query to `ElementQuery` inputs before passing them to this extractor.
+    *
+    * @param cssQuery the CSS query to apply to input `ElementQuery` instances
+    * @return an `HtmlExtractor` returning the contents extracted by this extractor after `cssQuery` is applied to the
+    *         input queries.
+    */
   def mapQuery(cssQuery: String): HtmlExtractor[E, A] =
-    HtmlExtractor { doc => extract(doc.select(cssQuery)) }
+    HtmlExtractor { q => extract(q.select(cssQuery)) }
 }
 
+/**
+  * The companion object for `HtmlExtractor`, containing methods for creating new extractors.
+  */
 object HtmlExtractor extends HtmlExtractorInstances {
 
+  /**
+    * Creates a new `HtmlExtractor` from a function.
+    *
+    * @param f the function used to extract content from an `ElementQuery`
+    * @tparam E the type of the elements needed by the `HtmlExtractor`
+    * @tparam A the type of the extracted content
+    * @return a new `HtmlExtractor` that extracts content using `f`.
+    */
   def apply[E <: Element, A](f: ElementQuery[E] => A): HtmlExtractor[E, A] = new HtmlExtractor[E, A] {
-    def extract(doc: ElementQuery[E]): A = f(doc)
+    def extract(q: ElementQuery[E]): A = f(q)
   }
 
+  /**
+    * Creates a new `HtmlExtractor` that extracts the elements of the input that match a CSS query.
+    *
+    * @param cssQuery the CSS query to apply
+    * @tparam E the type of the elements needed by the `HtmlExtractor`
+    * @return a new `HtmlExtractor` that extracts the elements of the input that match a CSS query.
+    */
   def forQuery[E <: Element](cssQuery: String): HtmlExtractor[E, ElementQuery[E]] =
     HtmlExtractor(_.select(cssQuery))
 }
@@ -31,11 +75,11 @@ trait HtmlExtractorInstances {
 
   implicit def extractorMonad[E <: Element] = new Monad[({ type t[A] = HtmlExtractor[E, A] })#t] {
     def point[A](a: => A) = new HtmlExtractor[E, A] {
-      def extract(doc: ElementQuery[E]) = a
+      def extract(q: ElementQuery[E]) = a
     }
 
     def bind[A, B](fa: HtmlExtractor[E, A])(f: A => HtmlExtractor[E, B]) = new HtmlExtractor[E, B] {
-      def extract(doc: ElementQuery[E]) = f(fa.extract(doc)).extract(doc)
+      def extract(q: ElementQuery[E]) = f(fa.extract(q)).extract(q)
     }
 
     override def map[A, B](fa: HtmlExtractor[E, A])(f: A => B) = fa.map(f)
@@ -49,7 +93,7 @@ case class SimpleExtractor[-E <: Element, C, +A] @deprecated(
     contentExtractor: ElementQuery[E] => C,
     contentParser: C => A) extends HtmlExtractor[E, A] {
 
-  def extract(doc: ElementQuery[E]) = contentParser(contentExtractor(doc.select(cssQuery)))
+  def extract(q: ElementQuery[E]) = contentParser(contentExtractor(q.select(cssQuery)))
 
   def withQuery(cssQuery: String) = copy(cssQuery = cssQuery)
 
--- core/src/main/scala/net/ruippeixotog/scalascraper/scraper/PolyHtmlExtractor.scala
+++ core/src/main/scala/net/ruippeixotog/scalascraper/scraper/PolyHtmlExtractor.scala
@@ -2,17 +2,50 @@ package net.ruippeixotog.scalascraper.scraper
 
 import net.ruippeixotog.scalascraper.model.Element
 
+/**
+  * An extractor like [[HtmlExtractor]] but whose extracted content type depends on the type of the input
+  * [[net.ruippeixotog.scalascraper.model.Element]]s. A `PolyHtmlExtractor` supports application of CSS queries and can
+  * be turned into a normal `HtmlExtractor` by calling its `apply[E]` method, fixing the type of the input `Element` as
+  * `E`.
+  */
 trait PolyHtmlExtractor { outer =>
+
+  /**
+    * The type of the extracted content as a function of the input elements.
+    *
+    * @tparam E the type of the input elements
+    */
   type Out[E <: Element]
 
+  /**
+    * Returns an `HtmlExtractor` obtained from this extractor by specifying the type of the input elements.
+    *
+    * @tparam E the type of the input elements
+    * @return an `HtmlExtractor` obtained from this extractor by specifying the type of the input elements.
+    */
   def apply[E <: Element]: HtmlExtractor[E, Out[E]]
 
+  /**
+    * Applies a CSS query to `ElementQuery` inputs before passing them to this extractor.
+    *
+    * @param cssQuery the CSS query to apply to input `ElementQuery` instances
+    * @return a `PolyHtmlExtractor` returning the contents extracted by this extractor after `cssQuery` is applied to
+    *         the input queries.
+    */
   def mapQuery(cssQuery: String): PolyHtmlExtractor.Aux[Out] = new PolyHtmlExtractor {
     type Out[E <: Element] = outer.Out[E]
     def apply[E <: Element] = outer[E].mapQuery(cssQuery)
   }
 
-  // TODO try to find a way to make this a DSL extension method. If that really can't be done, consider deprecating this
+  /**
+    * Applies a CSS query to `ElementQuery` inputs before passing them to this extractor.
+    *
+    * @param cssQuery the CSS query to apply to input `ElementQuery` instances
+    * @return a `PolyHtmlExtractor` returning the contents extracted by this extractor after `cssQuery` is applied to
+    *         the input queries.
+    */
+  // TODO try to find a way to make this a DSL extension method. If that really can't be done, consider deprecating
+  // this DSL construction
   def apply(cssQuery: String): PolyHtmlExtractor.Aux[Out] = mapQuery(cssQuery)
 }
 
