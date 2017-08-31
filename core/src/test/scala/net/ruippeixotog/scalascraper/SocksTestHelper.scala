66c89cbe987d868db3e759a5d25468f21fdd1380
Rui Gonçalves <ruippeixotog@gmail.com>
2017-08-31 22:51:29 +0100

    Improve SocksTestHelper documentation

--- core/src/test/scala/net/ruippeixotog/scalascraper/SocksTestHelper.scala
+++ core/src/test/scala/net/ruippeixotog/scalascraper/SocksTestHelper.scala
@@ -5,10 +5,16 @@ import java.net.{ ConnectException, Socket, SocketException }
 import org.specs2.matcher.ThrownMessages
 import org.specs2.mutable.Specification
 
+// The tests wrapped by `skipIfProxyUnavailable` are executed only if an external SOCKS proxy server is running. By
+// default it tries to connect to localhost:9050; if no service is listening in that endpoint, the test is skipped.
+//
+// The host and port of the SOCKS server can be changed by setting the TEST_SOCKS_PROXY_HOST and TEST_SOCKS_PROXY_PORT
+// ports. Any proxy may be used, but tests usually expect the proxy server to execute the request from a different
+// external IP (i.e. proxies running on the LAN doesn't work for testing purposes).
 trait SocksTestHelper extends ThrownMessages { this: Specification =>
 
-  val socksProxyHost = Option(System.getenv("SOCKS_PROXY_HOST")).getOrElse("localhost")
-  val socksProxyPort = Option(System.getenv("SOCKS_PROXY_PORT")).fold(9050)(_.toInt)
+  val socksProxyHost = Option(System.getenv("TEST_SOCKS_PROXY_HOST")).getOrElse("localhost")
+  val socksProxyPort = Option(System.getenv("TEST_SOCKS_PROXY_PORT")).fold(9050)(_.toInt)
 
   def skipIfProxyUnavailable[A](block: => A): A = {
     try {
--- core/src/test/scala/net/ruippeixotog/scalascraper/util/ProxyUtilsSpec.scala
+++ core/src/test/scala/net/ruippeixotog/scalascraper/util/ProxyUtilsSpec.scala
@@ -5,12 +5,6 @@ import org.specs2.mutable.Specification
 import net.ruippeixotog.scalascraper.SocksTestHelper
 import net.ruippeixotog.scalascraper.browser._
 
-// This test works only if an external Tor client is running. It tries to connect to localhost:9050; if no service is
-// listening in that endpoint, the test is skipped. The test makes a request to http://ipv4.icanhazip.com and the IPs
-// returned with and without proxy are compared.
-//
-// The host and port of the Tor server can be changed by setting the TOR_CLIENT_HOST and TOR_CLIENT_PORT ports. Other
-// SOCKS proxies may be used, as long as they execute the request from another external IP.
 class ProxyUtilsSpec extends Specification with BrowserHelper with SocksTestHelper {
   sequential
 
